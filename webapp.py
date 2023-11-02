f3a5fc18d3cf7426324dc21ac5d9594cd385fc40
starovit <ivanstarovit@gmail.com>
2023-11-02 15:37:48 +0200

    code webapp.py refactored

--- webapp.py
+++ webapp.py
@@ -37,6 +37,7 @@ if option == "File":
         main_loop(file)
 if option == "Camera":
     file = col1.camera_input(label="Please take a photo")
+    st.text(type(file))
     if file is not None:
         pil_object = Image.open(file).convert("RGB")
         col1.image(pil_object)
